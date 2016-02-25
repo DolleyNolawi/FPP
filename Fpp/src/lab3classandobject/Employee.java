465bbceba185e05d6f617fcf91f1aa832f4d20ce
DolleyNolawi <mer.world@gmail.com>
2016-02-25 08:58:35 -0600

    Locale

--- Fpp/src/lab3classandobject/Employee.java
+++ Fpp/src/lab3classandobject/Employee.java
@@ -1,22 +1,30 @@
 package lab3classandobject;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+
 
 public class Employee {
 	private String name;
 	private String nickName;
 	private double salary;
 	private Date hireDay;
+	//public static LocalDate hireDay;
 
 	// constructor
 	Employee(String name, String aNickName, double aSalary, int aYear,int aMonth, int aDay) {
 		this.name = name;
 		nickName = aNickName;
 		salary = aSalary;
-		GregorianCalendar cal = new GregorianCalendar(aYear, aMonth - 1, aDay);
+		GregorianCalendar cal = new GregorianCalendar(aYear, aMonth - 1, aDay);		
 		hireDay = cal.getTime();
 	}
-
+	
+/*	public static LocalDate LocalDateFromGregCalendar(GregorianCalendar cal) {
+		return LocalDate.of(cal.get(Calendar.YEAR), 1 + cal.get(Calendar.MONTH),
+		cal.get(Calendar.DATE));
+	}*/
+	
 	// instance methods
 	public String getName() {
 		return name;
